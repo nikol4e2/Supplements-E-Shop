0414ec090f4b11e167ec1d0288792b5c161af818
Nikola Arsov <arsovnikola1@gmail.com>
2023-06-19 23:45:12 +0200

    initial project

---
+++ src/main/java/com/shop/eshopapp/EShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.shop.eshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EShopAppApplication.class, args);
+    }
+
+}
